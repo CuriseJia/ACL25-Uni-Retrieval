6274a3f94c2b27489251f72802b7cc1311f1fe53
JiaYanhao <2474840061@qq.com>
2023-07-24 23:09:15 +0800

    0.0.2

--- main_1gpu.py
+++ main_1gpu.py
@@ -2,11 +2,12 @@ import argparse
 import os
 from tqdm import tqdm
 import torch
+import numpy as np
 from torch.utils.data import DataLoader
 
 from src.prompt.model import ShallowPromptTransformer
 from src.prompt.data import OriginalLongDataset
-from src.prompt.utils import init_distributed_mode, setup_seed, get_rank, get_world_size, is_main_process
+from src.prompt.utils import init_distributed_mode, setup_seed, get_rank, get_world_size, is_main_process, save_loss
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Parse args for SixModal Prompt Tuning.')
@@ -19,7 +20,7 @@ def parse_args():
     parser.add_argument('--seed', default=42, type=int)
     parser.add_argument('--world_size', default=1, type=int, help='number of distributed processes')    
     parser.add_argument('--dist_url', default='env://', help='url used to set up distributed training')
-    parser.add_argument('--distributed', default=True, type=bool)
+    parser.add_argument('--distributed', default=False, type=bool)
     parser.add_argument('--num_workers', default=4, type=int)
     parser.add_argument("--local_rank", type=int)
 
@@ -42,47 +43,59 @@ def parse_args():
     return args
 
 
+
 def train(args, model, device, dataloader, optimizer):
     model.train()
 
     best_loss = 10000000
 
-    for epoch in range(0, args.epochs):
+    losses = []
+    epoches = []
+
+    for epoch in tqdm(range(args.epochs)):
         if args.distributed:
             dataloader.sampler.set_epoch(epoch)
+        
+        temp_loss = []
 
         for data in enumerate(tqdm(dataloader)):
-            # print(data)
             image = data[1][0].to(device, non_blocking=True)
-            long_caption = model.module.tokenizer(data[1][1]).to(device, non_blocking=True)
+            # image = torch.zeros((data[1][0].shape), dtype=torch.float32).to(device, non_blocking=True)
+            long_caption = model.tokenizer(data[1][1]).to(device, non_blocking=True)
             negative_feature = data[1][2].to(device, non_blocking=True)
 
             image_feature = model(image, dtype='image')
             text_feature = model(long_caption, dtype='text')
 
-            loss = model.module.triplet_loss(image_feature, text_feature, negative_feature)
+            loss = model.triplet_loss(image_feature, text_feature, negative_feature)
 
-            # print("loss: {:.6f}".format(loss))
+            temp_loss.append(loss.detach().cpu().numpy())
+
+            print("loss: {:.6f}".format(loss))
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+        
+        res = round(sum(temp_loss)/len(temp_loss), 6)
+        print("epoch_{} loss is {}.".format(epoch, res))
+        losses.append(res)
+        epoches.append(epoch)
 
-        if is_main_process():
-            if loss<best_loss:
-                save_obj = model.module.state_dict()
-                torch.save(save_obj, os.path.join(args.output_dir, 'epoch_{}.path'.format(epoch)))
-
-
-
-
-
-
+        if res<best_loss:
+            best_loss = res
+            save_obj = model.state_dict()
+            torch.save(save_obj, os.path.join(args.output_dir, 'epoch_{}.pth'.format(epoch)))
+        
+        if best_loss < 0.0001:
+            break
+    
+    return losses, epoches
 
 
 if __name__ == "__main__":
     args = parse_args()
-    init_distributed_mode(args)
+    # init_distributed_mode(args)
     setup_seed(args.seed)
     device = torch.device(args.device)
 
@@ -96,22 +109,25 @@ if __name__ == "__main__":
             {'params': model.openclip.parameters(), 'lr': args.clip_ln_lr},
             {'params': [model.img_prompt], 'lr': args.prompt_lr}])
 
-    if args.distributed:
-        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu], find_unused_parameters=True)
-        # model_without_ddp = model.module 
-        num_tasks = get_world_size()
-        global_rank = get_rank()          
-        sampler = torch.utils.data.DistributedSampler(train_dataset, num_replicas=num_tasks, rank=global_rank, shuffle=True)
+    # if args.distributed:
+    #     model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu], find_unused_parameters=True)
+    #     # model_without_ddp = model.module 
+    #     num_tasks = get_world_size()
+    #     global_rank = get_rank()          
+    #     sampler = torch.utils.data.DistributedSampler(train_dataset, num_replicas=num_tasks, rank=global_rank, shuffle=True)
 
     train_loader = DataLoader(dataset=train_dataset, 
                             batch_size=args.batch_size,
                             num_workers=args.num_workers,
                             pin_memory=True,
-                            sampler=sampler,
+                            prefetch_factor=16,
+                            #sampler=sampler,
                             shuffle=False,
                             drop_last=True
                             )
 
     
 
-    train(args, model, device, train_loader, optimizer)
+    loss, epochs = train(args, model, device, train_loader, optimizer)
+
+    save_loss(loss, epochs)
